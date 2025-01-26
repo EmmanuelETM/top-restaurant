fb0bd66aaf49d8f306a98d1035a79b5f9dae7625
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-01-26 16:15:45 -0400

    Added background image, and position more stuff

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg)$/i,
-                type: "asset/resources",
+                type: "asset/resource",
             }
         ],
     }
